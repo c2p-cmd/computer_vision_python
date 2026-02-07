f88bda7d9462e927cf0b186113a437adf31d670d
Sharan Thakur <sharandt19@live.com>
2026-02-07 16:44:30 +0100

    Enhance face detection functionality with grayscale processing and improved scaling

--- opencv_learning/vision_applications/live_face_detection_cpp/face_detection.cpp
+++ opencv_learning/vision_applications/live_face_detection_cpp/face_detection.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void detectFace(cv::CascadeClassifier &faceClassifier, cv::CascadeClassifier &eyeClassifier, cv::Mat &image);
+void detectFace(cv::CascadeClassifier &faceClassifier, cv::CascadeClassifier &eyeClassifier, cv::Mat &image, cv::Mat &imageGray);
 
 void drawFPS(double const fps, cv::Mat &image);
 
@@ -33,10 +33,14 @@ int main()
 
     cout << "Face and eyes Classifier Loaded!" << endl;
 
-    cv::Mat frame;
+    cv::Mat frame, frameGray;
     using clock = chrono::high_resolution_clock;
     auto lastTime = clock::now();
     double fps = 0.0;
+
+    const int flags = cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO;
+    cv::namedWindow("Face Detection", flags);
+    cv::namedWindow("Face Detection Grayscale", flags);
 
     while (capture.isOpened())
     {
@@ -52,12 +56,13 @@ int main()
         fps = 1.0 / elapsed;
 
         // detect face
-        detectFace(faceClassifier, eyesClassifier, frame);
+        detectFace(faceClassifier, eyesClassifier, frame, frameGray);
 
         // draw fps
         drawFPS(fps, frame);
 
         cv::imshow("Face Detection", frame);
+        cv::imshow("Face Detection Grayscale", frameGray);
 
         auto k = cv::waitKey(1);
 
@@ -79,19 +84,26 @@ void drawFPS(double const fps, cv::Mat &image)
     cv::putText(image, message, cv::Point(20, 40), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 255, 0), 2);
 }
 
-void detectFace(cv::CascadeClassifier &faceClassifier, cv::CascadeClassifier &eyeClassifier, cv::Mat &image)
+void detectFace(cv::CascadeClassifier &faceClassifier, cv::CascadeClassifier &eyeClassifier, cv::Mat &image, cv::Mat &imageGray)
 {
     cv::Mat faceImageGray;
     cv::cvtColor(image, faceImageGray, cv::COLOR_BGR2GRAY);
-    // cv::equalizeHist(faceImageGray, faceImageGray);
+    const double scaleDown = 0.5;
+    cv::resize(faceImageGray, faceImageGray, cv::Size(), scaleDown, scaleDown, cv::INTER_LINEAR);
+    cv::equalizeHist(faceImageGray, faceImageGray);
 
     vector<cv::Rect> faces;
     faceClassifier.detectMultiScale(faceImageGray, faces, 1.2, 5, 0, cv::Size(30, 30));
 
     for (const auto &face : faces)
     {
+        // Scale face coordinates back to original image size
+        cv::Rect scaledFace = cv::Rect(
+            face.x / scaleDown, face.y / scaleDown,
+            face.width / scaleDown, face.height / scaleDown);
+
         // draw rect on face
-        rectangle(image, face, cv::Scalar(255, 255, 255), 2);
+        rectangle(image, scaledFace, cv::Scalar(255, 255, 255), 2);
 
         // draw circle on eyes
         cv::Mat faceROI = faceImageGray(cv::Rect(face.x, face.y, face.width, face.height / 2));
@@ -101,9 +113,12 @@ void detectFace(cv::CascadeClassifier &faceClassifier, cv::CascadeClassifier &ey
 
         for (const auto &eye : eyes)
         {
-            cv::Point center(face.x + eye.x + eye.width / 2, face.y + eye.y + eye.height / 2);
-            int radius = eye.width / 2;
+            const cv::Point center = cv::Point(
+                (face.x + eye.x + eye.width / 2) / scaleDown,
+                (face.y + eye.y + eye.height / 2) / scaleDown);
+            const int radius = (eye.width / 2) / scaleDown;
             cv::circle(image, center, radius, cv::Scalar(200, 100, 100), 2, cv::LINE_AA);
         }
     }
+    faceImageGray.copyTo(imageGray);
 }
